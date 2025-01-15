5d00bf621fe3e33bd7bc72d89dcc72420aaddbe0
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2025-01-16 00:38:19 +0530

    dashboard card widget added

--- lib/views/daily/daily_view.dart
+++ lib/views/daily/daily_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/utils/utils.dart';
+import 'package:task_manager/views/daily/dashboard_card_widget.dart';
 
 class DailyView extends StatelessWidget {
   const DailyView({super.key});
@@ -9,9 +11,30 @@ class DailyView extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: const Text('Todos'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add,
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('Daily view'),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Utils.horizontalSpace(100),
+            const DashboardCardWidget(),
+            const Center(
+              child: Text('Daily view'),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/daily/dashboard_card_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/utils/utils.dart';
+
+class DashboardCardWidget extends StatelessWidget {
+  const DashboardCardWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: Utils.width,
+      height: Utils.heightPer(20),
+      decoration: BoxDecoration(
+        // border: Border.all(color: Colors.grey),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 2,
+            offset: Offset(1, 1),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Todo Done',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                'Keep it up',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: Utils.widthPer(35),
+            height: Utils.widthPer(35),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: Colors.redAccent,
+            ),
+            child: const Center(
+              child: Text(
+                '3/6',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
